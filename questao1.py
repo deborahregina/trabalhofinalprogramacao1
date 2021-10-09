bb206d31fb5ae80d14d6e5d5b26a513985c3e1ec
deborah <deborahregs@gmail.com>
2021-10-09 18:27:50 -0300

    questão 1

---
+++ questao1.py
@@ -0,0 +1,27 @@
+opcao = 1
+
+
+def imprimirinfos(nomeCrianca, idadeCrianca, grauInstrucao):
+    print('A criança ' + nomeCrianca + ' tem ' + idadeCrianca + ' anos e está em ' + grauInstrucao)
+
+
+while opcao != '0':
+    nomeCrianca = input('Nome da criança: ')
+    idadeCrianca = int(input('Idade da criança: '))
+    if (idadeCrianca < 1):
+        print('Digite uma idade válida.')
+    if (idadeCrianca >= 1 and idadeCrianca <= 5):
+        grauinstrucao = 'Educação Infantil'
+        imprimirinfos(nomeCrianca, str(idadeCrianca), grauinstrucao)
+    elif (idadeCrianca >= 6 and idadeCrianca <= 10):
+        grauinstrucao = 'Ensino Fundamental I'
+        imprimirinfos(nomeCrianca, str(idadeCrianca), grauinstrucao)
+    elif (idadeCrianca >= 11 and idadeCrianca <= 14):
+        grauinstrucao = 'Ensino Fundamental II'
+        imprimirinfos(nomeCrianca, str(idadeCrianca), grauinstrucao)
+    elif (idadeCrianca >= 15):
+        grauinstrucao = 'Ensino médio'
+        imprimirinfos(nomeCrianca, str(idadeCrianca), grauinstrucao)
+
+    print('Deseja continuar?  0 - Não   1 - Sim')
+    opcao = input()
